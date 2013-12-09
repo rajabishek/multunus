01f0e0fc7b254adce8f82610b9fe9e1cf4b49260
Rohit Kumar <rohitt.k@multunus.com>
2013-12-09 14:32:46 +0530

    finish footer   logo and copyright   facebook like bottom

--- wp-content/themes/multunus/footer.php
+++ wp-content/themes/multunus/footer.php
@@ -1,12 +1,77 @@
-		<div id="footer">
-			&copy;<?php echo date("Y"); echo " "; bloginfo('name'); ?>
-		</div>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-4 col-xs-6">
+        <div class="align-center heading">Say hello!</div>
+          <a class="hexagon" href="#" title="Contact us">
+            <div class="title">Drop us a line</div>
+            <img src="/img/mail-icon-red.png"/>
+          </a>
+        </div>
 
-	</div>
+      <div class="col-sm-4 col-xs-6">
+        <div class="align-center heading">Career</div>
+        <a class="hexagon" href="#" title="Contact us">
+          <div class="title">Let's Build</div>
+          <img src="/img/robot-icon-red.png">
+        </a>
+      </div>
 
-	<?php wp_footer(); ?>
+      <div class="col-sm-4 col-xs-12">
+        <div class="follow-us">
+          <div class="align-center heading">Stay Updated</div>
+          <div class="hexagon" href="#" title="Contact us">
+            <div class="title">Follow us</div>
+
+            <ul class="list-inline align-center">
+              <li>
+                <a class="social" href="" title="Follow Multunus on Facebook">
+                <img src="/img/facebook-icon-footer.png" alt=""/>
+                </a>
+              </li>
+
+              <li>
+                <a class="social" href="" title="Follow Multunus on Twitter">
+                <img src="/img/twitter-icon-footer.png" alt=""/>
+                </a>
+              </li>
+
+              <li>
+                <a class="social" href="" title="Follow Multunus on Google Plus">
+                <img src="/img/google-plus-icon-footer.png" alt=""/>
+                </a>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+
+    </div> <!-- .row end -->
+
+    <div class="row">
+      <div class="bottom">
+        <hr class="hidden-xs"/>
+        <div class="col-xs-12 col-sm-11 copyright">
+          <p>
+            <a class="logo-footer" href="">
+              <img src="/img/multunus-logo-footer.png">
+            </a>
+            All rights reserved. <?php echo date("Y"); echo ", "; bloginfo('name'); ?>
+          </p>
+        </div>
+
+        <div class="facebook-like col-sm-1 hidden-xs pull-right">
+          <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2Fmultunus&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21&amp;appId=176869535839438" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>
+        </div>
+      </div>
+    </div> <!-- .row end -->
+  </div>  <!-- .container end -->
+</footer>
+
+<?php wp_footer(); ?>
 	
-	<!-- Don't forget analytics -->
+<!-- Don't forget analytics -->
 	
 </body>
 
