da5b487325b9ffcab98d66c931c4a8d381f8fd7e
Rohit Kumar <rohitt.k@multunus.com>
2013-12-09 14:58:54 +0530

    home.php   full screen video, autoplay   text overlay to video

---
+++ wp-content/themes/multunus/home.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div class="video-section-container">
+  <div class="overlay">
+    <div class="overlay-content">
+      <h1 id="quote">We help entrepreneurs succeed</h1>
+      <div class="hidden-xs">
+        <a href="#main-content" class="btn_red">explore<span></span></a>
+      </div>
+    </div>
+  </div>
+
+  <video tabindex="0" autoplay loop>
+    <source src="/vids/1.mp4" type="video/mp4" />
+    <source src="/vids/2.mp4" type="video/mp4" />
+  </video>
+</div>
+
+<?php get_footer(); ?>
