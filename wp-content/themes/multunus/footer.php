e5f1cfe8ee77328b00e17fa093c24d03eedbaf85
Rohit Kumar <rohitt.k@multunus.com>
2013-12-11 12:10:14 +0530

    fluid footer on all device size using media queries

--- wp-content/themes/multunus/footer.php
+++ wp-content/themes/multunus/footer.php
@@ -2,47 +2,45 @@
   <div class="container">
     <div class="row">
       <div class="col-sm-4 col-xs-6">
-        <div class="align-center heading">Say hello!</div>
-          <a class="hexagon" href="#" title="Contact us">
-            <div class="title">Drop us a line</div>
-            <img src="/img/mail-icon-red.png"/>
-          </a>
-        </div>
-
-      <div class="col-sm-4 col-xs-6">
-        <div class="align-center heading">Career</div>
-        <a class="hexagon" href="#" title="Contact us">
-          <div class="title">Let's Build</div>
-          <img src="/img/robot-icon-red.png">
-        </a>
+          <div class="align-center heading">Say hello!</div>
+          <div class="hexagon">
+            <div class="title">Drop a line</div>
+            <a class="mail-container" href="mailto:info@multunus.com"><img src="/img/mail-icon-red.png"/></a>
+          </div>
       </div>
 
-      <div class="col-sm-4 col-xs-12">
+      <div class="col-sm-4 col-xs-6">
+          <div class="align-center heading">Career</div>
+          <div class="hexagon">
+            <div class="title">Let's Build</div>
+            <a href="/careers"><img src="/img/robot-icon-red.png"></a>
+          </div>
+      </div>
+
+      <div class="col-sm-4 col-xs-12 follow-us-container">
         <div class="follow-us">
           <div class="align-center heading">Stay Updated</div>
-          <div class="hexagon" href="#" title="Contact us">
+          <div class="hexagon">
             <div class="title">Follow us</div>
-
             <ul class="list-inline align-center">
               <li>
-                <a class="social" href="" title="Follow Multunus on Facebook">
-                <img src="/img/facebook-icon-footer.png" alt=""/>
+                <a class="social" href="https://www.facebook.com/multunus" title="Follow us on Facebook">
+                  <img src="/img/facebook-icon-footer.png" alt=""/>
                 </a>
               </li>
 
               <li>
-                <a class="social" href="" title="Follow Multunus on Twitter">
-                <img src="/img/twitter-icon-footer.png" alt=""/>
+                <a class="social" href="https://twitter.com/multunus" title="Follow us on Twitter">
+                  <img src="/img/twitter-icon-footer.png" alt=""/>
                 </a>
               </li>
 
               <li>
-                <a class="social" href="" title="Follow Multunus on Google Plus">
-                <img src="/img/google-plus-icon-footer.png" alt=""/>
+                <a class="social" href="" title="Follow us on G+">
+                  <img src="/img/google-plus-icon-footer.png" alt=""/>
                 </a>
               </li>
             </ul>
-
           </div>
         </div>
       </div>
@@ -62,7 +60,7 @@
         </div>
 
         <div class="facebook-like col-sm-1 hidden-xs pull-right">
-          <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Ffacebook.com%2Fmultunus&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21&amp;appId=176869535839438" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:21px;" allowTransparency="true"></iframe>
+          <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;width=100&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21&amp;appId=176869535839438" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:21px;" allowTransparency="true"></iframe>
         </div>
       </div>
     </div> <!-- .row end -->
