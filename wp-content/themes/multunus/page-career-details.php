13c6d908f3501535911dcfe6168d06c4fcdbf8c7
Rohit Kumar <rohitt.k@multunus.com>
2013-12-24 19:55:55 +0530

    initial layout of the FAQ page

--- wp-content/themes/multunus/page-career-details.php
+++ wp-content/themes/multunus/page-career-details.php
@@ -20,42 +20,71 @@
   </video>
 </div>
 
-<section id="career-details" class="career-details">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 career-details-content">
-        <h1><?php the_title(); ?></h1>
-        <p><?php the_content(); ?></p>
-
-        <aside class="career-contact">
-          <h5>Interested</h5>
-          <p>Send a little something about yourself to <a href="mailto:jobs@multunus.com">jobs@multunus.com</a></p>
-        </aside>
-      </div>
-
-      <div class="col-md-3 col-md-offset-1 career-details-sidebar">
-        <h4>Current Openings</h4>
-        <ul class="current-openings">
-        <li>Programmer</li>
-
-        <?php
+<div class="container current-openings-dropdown visible-xs">
+  <div class="dropdown-label">Current Openings: </div>
+  <div class="btn-group dropdown">
+    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+      Programmer 
+    </button>
+    <ul class="dropdown-menu" role="menu">
+      <?php
+        $page_id = get_queried_object_id();
         if ($childrens = get_children('post_parent=40&post_type=page')):
           foreach ($childrens as $children):
             $post = get_post($children->ID);
             $title = $post->post_title;
             $permalink = post_permalink($children->ID);
-        ?>
+            if ($page_id == $children->ID):
+              echo '<li class="active"><a href="' . $permalink . '">' . $title . '</a></li>';
+            else:
+              echo '<li><a href="' . $permalink . '">' . $title . '</a></li>';
+            endif;
+          endforeach;
+        endif;
+      ?>
+    </ul>
+  </div>
+</div><!-- end of container -->
 
-          <li><a href="<?php echo $permalink ?>"><?php echo $title ?></a></li>
-        <?php endforeach; ?>
-        <?php endif; ?>
-        </ul>
+<section id="career-details" class="career-details">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 career-details-content">
+        <h1 class="position-title"><?php the_title(); ?></h1>
+        <p><?php the_content(); ?></p>
+
+        <aside class="career-contact">
+          <h5>Interested</h5>
+          <p class="hidden-xs">Send a little something about yourself to <a href="mailto:jobs@multunus.com">jobs@multunus.com</a></p>
+          <p class="visible-xs">Send a little  about yourself to <br /><a href="mailto:jobs@multunus.com">jobs@multunus.com</a></p>
+        </aside>
+      </div>
+
+      <div class="col-md-3 col-md-offset-1 career-details-sidebar">
+        <div class="hidden-xs">
+          <h4>Current Openings</h4>
+          <ul class="current-openings">
+          <li>Programmer</li>
+
+          <?php
+          if ($childrens = get_children('post_parent=40&post_type=page')):
+            foreach ($childrens as $children):
+              $post = get_post($children->ID);
+              $title = $post->post_title;
+              $permalink = post_permalink($children->ID);
+          ?>
+
+            <li><a href="<?php echo $permalink ?>"><?php echo $title ?></a></li>
+          <?php endforeach; ?>
+          <?php endif; ?>
+          </ul>
+        </div>
 
         <h4>How We Hire</h4>
         <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames</p>
 
         <h4>FAQ</h4>
-        <ol class="career-faq">
+        <ol class="career-details-faq">
           <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</li>
           <p>onsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
             aliqua. Ut enim ad minim veniam,
@@ -64,7 +93,8 @@
             aliqua. Ut enim ad minim veniam,
         </ol>
 
-        <a id="read-faq-more" href="#">Read All FAQ</a>
+        <?php $permalink = get_permalink( 4228 ); ?> 
+        <a id="read-faq-more" href="<?php echo $permalink ?>">Read All FAQ</a>
       </div><!-- end of career-details-section -->
     </div><!-- end of row -->
   </div><!-- end of container -->
---
+++ wp-content/themes/multunus/page-faq.php
@@ -0,0 +1,78 @@
+<?php
+  /*
+   Template Name: FAQ
+   */
+?>
+
+<?php get_header(); ?>
+
+<section id="back-to-top" class="career-faq">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9">
+       <h1>Frequently Asked Question</h1>
+        <hgroup class="red-text clearfix">
+          <h2 class="pull-left">Company</h2>
+          <a class="pull-right go-to-top" href="#">Back to Top</a>
+        </hgroup>
+        <ol>
+          <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </li>
+           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+             amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </li>
+           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+             amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </li>
+           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+             amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        </ol>
+
+        <hgroup class="red-text clearfix">
+          <h2 class="pull-left">Project</h2>
+          <a class="pull-right go-to-top" href="#">Back to Top</a>
+        </hgroup>
+        <ol>
+          <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </li>
+           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+             amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </li>
+           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+             amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </li>
+           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+             amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        </ol>
+
+        <hgroup class="red-text clearfix">
+          <h2 class="pull-left">Career</h2>
+          <a class="pull-right go-to-top" href="#">Back to Top</a>
+        </hgroup>
+        <ol>
+          <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </li>
+           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+             amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </li>
+           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+             amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+          <li>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. </li>
+           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+            Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit
+             amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+        </ol>
+
+        <a class="go-to-top visible-xs" href="back-to-top">Back to Top</a>
+      </div>
+
+    </div><!-- end of row -->
+  </div><!-- end of container -->
+</section>
+
+<?php get_footer(); ?>
