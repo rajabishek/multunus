27c512b357a960599d6c3e05a831f4f7f76061c5
Akshay S Murthy <akshay.s@multunus.com>
2014-03-13 16:36:57 +0530

    changed blog footer text background

--- wp-content/themes/multunus/single.php
+++ wp-content/themes/multunus/single.php
@@ -39,9 +39,9 @@
           <?php $contact_page_permalink = get_permalink( get_page_by_path( 'contact' ) ); ?>
 
             <aside class="blog-footer-text">
-            <p>"Some unexpected bad things will happen" - Dan North.</p>
-            <p>We ship working software every 2 days. Its a great way to discover those unexpected bad things early on.</p>
-            <p>We build web and mobile apps. If you have one in mind, <a href="<?php echo $contact_page_permalink . "#main-content" ?>">talk to us.</a></p>
+            <p id="dan-north-quote">"Some unexpected bad things will happen" - Dan North.</p>
+            <p>We help you discover those unexpected bad things early on. By shipping working software every 2 days.</p>
+            <p>We build web and mobile apps. If you have one in mind, <a href="<?php echo $contact_page_permalink . "#main-content" ?>">talk to us</a>.</p>
             </aside>
 
 						<ul class="social-share visible-xs">
