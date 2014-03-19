2184f2b126c09c0d0e8c7d63be8a60f376164cbb
Rohit Kumar <rohitt.k@multunus.com>
2014-03-19 05:59:51 +0000

    refactor 3-column-layout from community and continous-delivery into a separate template

--- wp-content/themes/multunus/page-continuous-delivery.php
+++ wp-content/themes/multunus/page-continuous-delivery.php
@@ -26,31 +26,7 @@ get_header();
     <h2>We also <img src="/img/continuous-delivery/heart.png" class="heart" /> our community.</h2>
   </section>
 
-  <section id="links">
-    <div class="row">
-      <div class="col-md-4 link-box">
-        <h3><a href="/blog/category/continuous-delivery/">/Blog</a></h3>
-        <p>Read about our continuous delivery experience in iOS, Android and Rails.</p>
-      </div>
-      <div class="col-md-4 link-box">
-        <h3><a href="/events">/Events</a></h3>
-        <ul class="events">
-          <li id="android">
-            <div class="name">Speaking @ DroidSync 2014</div>
-            <div class="place">Mumbai</div>
-          </li>
-          <li id="rails">
-            <div class="name">Speaking @ RailsConf 2014</div>
-            <div class="place">Chicago</div>
-          </li>
-        </ul>
-      </div>
-      <div class="col-md-4 link-box">
-        <h3><a href="/open-source">/OpenSource</a></h3>
-        <p>Check out RubyMation! (iOS Animation Libraries in RubyMotion)</p>
-      </div>
-    </div>
-  </section>
+  <?php get_template_part('blog-events-opensource'); ?>
 
   <section id="platforms" class="align-center">
     <h5><a href="mailto:info@continuousdelivery.in">info@continuousdelivery.in</a></h5>
