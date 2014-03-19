fa3a057f6b15fb8a18ea1ee6eb4430682d88da00
Rohit Kumar <rohitt.k@multunus.com>
2014-03-19 05:48:05 +0000

    3 column layout on community page

--- wp-content/themes/multunus/page-community.php
+++ wp-content/themes/multunus/page-community.php
@@ -5,23 +5,37 @@
 get_header();
 ?>
 
-<div class="community">
-  <div class="jumbotron red-jumbo">
-    <div class="jumbo-content">
-      <ul class="container">
-        <li class="col-sm-4 col-xs-12">
-          <a href="/blog">Blog</a>
-        </li>
-        <li class="col-sm-4 col-xs-12">
-          <a href="/open-source">Open Source</a>
-        </li>
-        <li class="col-sm-4 col-xs-12">
-          <a href="/events">Events</a>
-        </li>
-      </ul>
-    </div>
-  </div>
+<div id="community" class="container">
+  <section id="community-love" class="align-center">
+    <h1>We <img src="/img/continuous-delivery/heart.png" class="heart" /> our community.</h1>
+  </section>
 
-</div><!-- end of community -->
+  <section id="links">
+    <div class="row">
+      <div class="col-md-4 link-box">
+        <h3><a href="/blog/category/continuous-delivery/">/Blog</a></h3>
+        <p>Read about our continuous delivery experience in iOS, Android and Rails.</p>
+      </div>
+      <div class="col-md-4 link-box">
+        <h3><a href="/events">/Events</a></h3>
+        <ul class="events">
+          <li id="android">
+            <div class="name">Speaking @ DroidSync 2014</div>
+            <div class="place">Mumbai</div>
+          </li>
+          <li id="rails">
+            <div class="name">Speaking @ RailsConf 2014</div>
+            <div class="place">Chicago</div>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-4 link-box">
+        <h3><a href="/open-source">/OpenSource</a></h3>
+        <p>Check out RubyMation! (iOS Animation Libraries in RubyMotion)</p>
+      </div>
+    </div>
+  </section>
+
+</div><!-- end of #community -->
 
 <?php get_footer(); ?>
