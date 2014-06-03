89ae85606a815ba4224943427e4a6bf6f0f10565
Sandeep Venugopal <sandeep.v@multunus.com>
2014-06-03 18:11:29 +0530

    display past events if no current/future events are there

--- wp-content/themes/multunus/blog-events-opensource.php
+++ wp-content/themes/multunus/blog-events-opensource.php
@@ -24,7 +24,7 @@
       <?php
         $today =  current_time ('timestamp');
 
-        $args = array(
+        $args_future = array(
           'post_type' => 'ajde_events',
           'meta_key' => 'evcal_srow',
           'orderby'   => 'meta_value_num',
@@ -40,9 +40,9 @@
           )
         );
 
-        $my_query = new WP_Query($args);
+        $future_query = new WP_Query($args_future);
 
-        while ($my_query->have_posts()) : $my_query->the_post();
+        while ($future_query->have_posts()) : $future_query->the_post();
 
           $start_time = get_post_meta(get_the_ID(), 'evcal_erow', true); ?>
 
@@ -56,6 +56,48 @@
           </div>
 
        <?php endwhile;  wp_reset_query(); ?>
+
+
+        <?php
+          $future_posts = $future_query->post_count;
+
+          if ($future_posts < 3); {
+
+            $num_left = 3 - $future_posts;
+
+            $args_past = array(
+              'post_type' => 'ajde_events',
+              'meta_key' => 'evcal_srow',
+              'orderby'   => 'meta_value_num',
+              'order' => 'DESC',
+              'post_status' => 'publish',
+              'posts_per_page' => $num_left,
+              'meta_query' => array(
+                 array(
+                   'key' => 'evcal_srow',
+                   'value' => $today,
+                   'compare' => '<',
+                 )
+              )
+            );
+
+            $past_query = new WP_Query($args_past);
+
+            echo '<h6><a href="/events">/Past Events</a></h6>';
+            while ($past_query->have_posts()) : $past_query->the_post();
+
+              $start_time = get_post_meta(get_the_ID(), 'evcal_erow', true); ?>
+
+              <div class="post-title blog">
+                <a href="<?php the_permalink(); ?>" title="Read more"><?php the_title(); ?>
+                  <br/>
+                  <span class="community-meta-data">
+                    <?php echo date('F d', $start_time); ?>
+                  </span>
+                </a>
+              </div>
+       <?php endwhile;  wp_reset_query(); ?>
+       <?php } ?>
 
       <a class="view-all-link" href="/events">View all</a>
 
