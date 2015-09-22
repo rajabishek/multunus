03a67a6dbe43f6ca7afef6029bbd48f6a80f420c
Ramyasree <ramyasree.d@multunus.com>
2015-09-22 19:50:05 +0530

    Changed copy in the banner and read more section

--- wp-content/themes/multunus/page-careers.php
+++ wp-content/themes/multunus/page-careers.php
@@ -8,29 +8,30 @@
 <div class="video-section-container career-page">
   <div class="overlay">
     <div class="overlay-content">
-      <h1 id="quote">3 Reasons to Work Here</h1>
-    <h2 style="color:white">
-      <span>Autonomous Culture | Open Source | Large Scale Architectures</span>
-       </h2>
-      <div data-toggle="scroll" rel="#open-positions" class="btn-red">Open Positions<span></span></div>
+      <h1 id="quote">What Should "Work" Mean Today?</h1>
+    <!-- <h2 style="color:white"> -->
+      <!-- <span>Autonomous Culture | Open Source | Large Scale Architectures</span> -->
+       <!-- </h2> -->
+      <div data-toggle="scroll" rel="#more-info" class="btn-red">Read More<span></span></div>
     </div>
   </div>
   <video autoplay="autoplay" loop="loop" preload="auto" poster="/img/careers-page-poster.jpeg" data-video-name="careers">
   </video>
 </div>
-<article class="career-values">
+<article id="more-info" class="career-values">
   <div class="container">
     <div class="row">
 
       <div class="col-md-4">
-        <h1>More info here:</h1>
+        <h1>What Should "Work" Mean Today?</h1>
       </div>
 
       <div class="col-md-7 why-work-here">
         <ul class="arrow-list-style">
-          <li><span><a href="https://github.com/multunus/Open-Playbook" target="_blank"> Our Compensation System</a></span></li>
-          <li><span><a href="http://www.multunus.com/community/open-source/" target="_blank"> Open Source Contributions</a></span></li>
-          <li><span><a href="http://www.multunus.com/portfolio/micro-loan-processing-platform/" target="_blank"> An Impactful Project</a></span></li>
+          <li><span> Making a difference | Charity & Health </span></li>
+          <li><span> Making a difference | Impactful Work </span></li>
+          <li><span> Making a difference | <a href="http://www.multunus.com/community/" target="_blank"> Community </a></span></li>
+          <li><span> Making a difference | Creating Better Teams </span></li>
         </ul>
       </div>
 
