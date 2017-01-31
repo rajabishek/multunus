202520e58c7fc4f08f80c7e65488204b72c5abaf
Raj Abishek <rajabishek@hotmail.com>
2017-01-31 18:01:45 +0530

    Adds the styling for the 404 page

--- wp-content/themes/multunus/404.php
+++ wp-content/themes/multunus/404.php
@@ -1,7 +1,32 @@
 <?php get_header(); ?>
-
-	<h2>Error 404 - Page Not Found</h2>
-
-<?php get_sidebar(); ?>
-
+<div class="lost-page-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>Oops!</h1>
+				<h3>We cant seem to find the page you are looking for.</h3>
+				<p>Here are some helpful links instead.</p>
+				<div class="row">
+					<div class="col-md-2">
+						<ul>
+							<li><a href="/">Home</a></li>
+							<li><a href="/services">Services</a></li>
+							<li><a href="/portfolio">Portfolio</a></li>
+							<li><a href="/contact">Contact Us</a></li>
+							<li><a href="/careers">Careers</a></li>
+						</ul>
+					</div>
+					<div class="col-md-2">
+						<ul>
+							<li><a href="/community">Community</a></li>
+							<li><a href="/community/open-source">Open Source</a></li>
+							<li><a href="/process">Process</a></li>
+							<li><a href="/team">Team</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <?php get_footer(); ?>
