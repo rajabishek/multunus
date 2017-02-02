80b01d7dd49354e6be940cd5be2d3225f5ac8b42
Raj Abishek <rajabishek@hotmail.com>
2017-02-02 17:57:43 +0530

    Adds the embedded form on the contact us page

--- wp-content/themes/multunus/page-contact.php
+++ wp-content/themes/multunus/page-contact.php
@@ -10,10 +10,7 @@
         <div class="contact-us">
         <h1 class="align-center">Hello there</h1>
         <p>We answer over 100 messages every week from our contact form, and we love talking to our potential customers, fans, haters and people who've got some feedback for us.</p>
-        
-        <!-- Change the width and height values to suit you best -->
-        <div class="typeform-widget" data-url="https://multunus-feedback.typeform.com/to/nGvoX3" data-text="Contact Us Form" style="width:100%;height:500px;"></div>
-        <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'widget.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
+        <iframe id="contact-typeform-container" src="https://multunus-feedback.typeform.com/to/nGvoX3"></iframe>
     </section>
 </div>
 <?php get_footer(); ?>
