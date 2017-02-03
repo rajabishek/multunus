ff380513f719125ecba94a09c51347d618fd2489
Raj Abishek <rajabishek@hotmail.com>
2017-02-03 10:55:25 +0530

    Increases the height of google maps

--- wp-content/themes/multunus/page-contact.php
+++ wp-content/themes/multunus/page-contact.php
@@ -28,7 +28,7 @@
         		</div>
         		<div class="col-sm-8">
         			<div class="google-maps-container">
-		    			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.956344752757!2d77.58674061546739!3d12.910527390896055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae150d8a6e03b1%3A0xc88ebcfe41394a8a!2sMultunus+Software+Pvt+Ltd!5e0!3m2!1sen!2sin!4v1486094729860" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen class="google-maps"></iframe>
+		    			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.956344752757!2d77.58674061546739!3d12.910527390896055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae150d8a6e03b1%3A0xc88ebcfe41394a8a!2sMultunus+Software+Pvt+Ltd!5e0!3m2!1sen!2sin!4v1486094729860" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen class="google-maps"></iframe>
 		    		</div>
         		</div>
         	</div>
