bf6d32cc48ce445e36b27c5d8c492c58e4717d83
Raj Abishek <rajabishek@hotmail.com>
2017-02-03 10:01:14 +0530

    Adds the google maps embedded

--- wp-content/themes/multunus/page-contact.php
+++ wp-content/themes/multunus/page-contact.php
@@ -7,10 +7,32 @@
 <?php get_header(); ?>
 <div class="contact-us-section">
     <section id="main-content" class="container">
-        <div class="contact-us">
         <h1 class="align-center">Hello there</h1>
         <p>We answer over 100 messages every week from our contact form, and we love talking to our potential customers, fans, haters and people who've got some feedback for us.</p>
-        <iframe id="contact-typeform-container" src="https://multunus-feedback.typeform.com/to/nGvoX3"></iframe>
+        <p>Just email us at <a href="mailto:info@multunus.com" class="email-link">info@multunus.com</a>, or fill in the form below.</p>
+        <div class="type-form-container">
+			<iframe id="contact-typeform" src="https://multunus-feedback.typeform.com/to/nGvoX3"></iframe>
+		</div>
+        <div class="clickable-card">
+        	<div class="row">
+        		<div class="col-sm-4">
+        			<div class="office-address-container">
+        				<address>
+			    			<strong>Multunus Software Pvt Ltd</strong><br>
+			    			1316/A, 9th Cross Rd,<br>
+			    			2nd Phase, JP Nagar,<br>
+			    			Bengaluru, Karnataka 560078<br>
+			    			<abbr title="Phone">Phone:</abbr> 080 6570 2964
+			    		</address>
+        			</div>
+        		</div>
+        		<div class="col-sm-8">
+        			<div class="google-maps-container">
+		    			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.956344752757!2d77.58674061546739!3d12.910527390896055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae150d8a6e03b1%3A0xc88ebcfe41394a8a!2sMultunus+Software+Pvt+Ltd!5e0!3m2!1sen!2sin!4v1486094729860" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen class="google-maps"></iframe>
+		    		</div>
+        		</div>
+        	</div>
+		</div>
     </section>
 </div>
 <?php get_footer(); ?>
