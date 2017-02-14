60a395c10e36dc4b8a5bb60fe2596876c2ce8297
Raj Abishek <rajabishek@hotmail.com>
2017-02-14 16:49:23 +0530

    Adds the lets talk section on the single portfolio

--- wp-content/themes/multunus/single-portfolio.php
+++ wp-content/themes/multunus/single-portfolio.php
@@ -140,6 +140,19 @@
             </div>
         </div>
     </section>
+    <div class="lets-talk">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2 class="text-center">Its time to craft brilliance together ? Let's chat.</h2>
+                    <div class="text-center">
+                        <a class="typeform-share link button button-white-filled" href="https://multunus-feedback.typeform.com/to/nGvoX3" data-mode="2" target="_blank">Get in touch with us today</a>
+                        <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm_share',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 <?php endwhile; endif; ?>
 <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
