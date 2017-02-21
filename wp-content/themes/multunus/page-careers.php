38438eea125bfee3ecf5231a625f01938f727bad
Raj Abishek <rajabishek@hotmail.com>
2017-02-21 15:48:35 +0530

    Removes the 4 day workweek section

--- wp-content/themes/multunus/page-careers.php
+++ wp-content/themes/multunus/page-careers.php
@@ -7,66 +7,13 @@
 <div class="video-section-container career-page">
     <div class="overlay">
         <div class="overlay-content">
-            <h1 id="quote">Freedom.</h1>
-            <!-- <h2 style="color:white"> -->
-            <!-- <span>Autonomous Culture | Open Source | Large Scale Architectures</span> -->
-            <!-- </h2> -->
-            <div data-toggle="scroll" rel="#more-info" class="button button-red-filled">Start with a 4 day workweek<span></span></div>
+            <h1 id="quote">Join Our Team.</h1>
+            <div data-toggle="scroll" rel="#open-positions" class="button button-red-filled">Find a role<span></span></div>
         </div>
     </div>
     <video autoplay="autoplay" loop="loop" preload="auto" poster="/img/careers-page-poster.jpeg" data-video-name="careers">
     </video>
 </div>
-<article id="more-info" class="career-values">
-    <div class="container">
-        <div class="row">
-            <div class="row design-container">
-                <div class="col-md-6 design-caption">
-                    <span class="design-heading">
-                        <h3>Choose a 4 day workweek</h3>
-                    </span>
-                    <div class="design-description">
-                        <p>
-                            Choose your favorite project. Work on it every Monday. <a href="http://www.multunus.com/blog/2016/01/20-investment-time-background-story/" target="_blank">Read more about our 20% investment time.</a>
-                        </p>
-                    </div>
-                </div>
-                <div class="design-list-container col-md-6" >
-                    <span class="design-heading">
-                        <h3>Choose our future
-                        </h3>
-                    </span>
-                    <div class="design-description">
-                        <p>
-                            Yep. <a href="http://www.multunus.com/blog/2016/01/relinquishing-control-to-our-council-a-radical-experiment/" target="_blank">Get elected into our council</a> and make CEO level decisions. Like..what our revenues should be. No kidding.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-6 design-caption">
-                    <span class="design-heading">
-                        <h3>Choose your salary
-                        </h3>
-                    </span>
-                    <div class="design-description">
-                        <p>
-                            Its 2017. It’s time to break the rules. Have fun using <a href="http://www.multunus.com/blog/2015/09/our-autonomous-salary-system-the-background-story-part-1/" target="_blank">our autonomous salary system.</a>
-                        </p>
-                    </div>
-                </div>
-                <div class="design-list-container col-md-6" >
-                    <span class="design-heading">
-                        <h3>Choose your future</h3>
-                    </span>
-                    <div class="design-description">
-                        <p>
-                            Want to code all your life? <a href="http://www.multunus.com/blog/2016/01/training-fish-climb-tree/" target="_blank">No problem</a>. Want to try out product management or think you’re a designer at heart? <a href="http://www.multunus.com/blog/2016/01/training-fish-climb-tree/" target="_blank"> Go for it</a>.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-</article>
-
 <article id="open-positions" class="career-openings">
     <div class="container">
         <h1>Open Positions</h1>
